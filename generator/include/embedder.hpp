a92ec1a83b4974e397b134586471b5bdd1b5b91c
KokaKiwi <kokakiwi+git@kokakiwi.net>
2022-03-04 18:22:22 +0100

    fix: Code generation for clang (#1)

--- generator/include/embedder.hpp
+++ generator/include/embedder.hpp
@@ -5,13 +5,13 @@ R"embedder(
 
     #define RESOURCE(name, path)                        \
     __asm__ (                                           \
-        ".global _" #name "\n"                          \
-        ".global _" #name "_size\n"                     \
-        "_" #name ":\n"                                 \
+        ".global " #name "\n"                          \
+        ".global " #name "_size\n"                     \
+        #name ":\n"                                 \
             ".incbin \"" path "\"\n"                    \
             ".align 8\n"                                \
-        "_" #name "_size:\n"                            \
-            ".int _" #name "_size - _" #name " - 1\n"   \
+        #name "_size:\n"                            \
+            ".int " #name "_size - " #name " - 1\n"   \
             ".align 8\n"                                \
     )
 
